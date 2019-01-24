eff88f734ac8b37bfd76d521aea14874afa7a5d7
abdurahmanfz <zandt55@gmail.com>
2019-01-24 22:30:07 +0700

    tambah method luas lingkaran

--- tugas1/HitungLuas.java
+++ tugas1/HitungLuas.java
@@ -3,9 +3,14 @@
 public class hitungLuas {
   static int cariLuasPersegiPanjang(int panjang, int lebar) {
     return panjang * lebar;
+	}
+	
+	static int cariLuasLingkaran(float r) {
+    return 3.15 * (r * 2);
   }
 
   public static void main(String[] args) {
-    System.out.println(myMethod(5, 3));
+    System.out.println("Luas Persegi" + cariLuasPersegiPanjang(5, 3));
+    System.out.println("Luas Lingkaran : " + cariLuasLingkaran(4));
   }
 }
