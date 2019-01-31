e93a09b2153247919634e9d44adb8228a038be4f
abdurahmanfz <zandt55@gmail.com>
2019-02-01 00:20:09 +0700

    pertemuan-3 (OOP)

---
+++ pertemuan-3/Latihan/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+  //Latihan overloading method
+  public static void main(String[] args) {
+    Mobil sedan = new Mobil();
+    sedan.start();
+    sedan.gas();
+    sedan.gantiGigi(2);
+    sedan.gas();
+    sedan.gantiGigi(3);
+    sedan.gas();
+    sedan.rem();
+    sedan.stop();
+    sedan.gas();
+  }
+}
